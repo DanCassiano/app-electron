d141d503a4c30ec778687ba0bf91058e86d243f1
DanCassiano <jordan_gnr@hotmail.com>
2015-11-18 21:46:19 -0200

    Novas funcoes no git

--- modulos/git.js
+++ modulos/git.js
@@ -20,8 +20,36 @@ exports.commit = function( titulo, msg, retorno ){
 	exec(' git commit -m "'+ titulo + '\s\n' + msg + '"' ,{ cwd: this.dir},function(error, stdout, stderr){
 		retorno(stdout);	
 		sys.puts(error) 
-
 	});		
 }
 
 
+exports.gitListContribuidores = function(retorno ){
+	
+	exec( 'git log --pretty=format:%an' ,{ cwd: this.dir},function(error, stdout, stderr){
+		retorno(stdout);	
+		puts( error, stdout, stderr );
+		console.log( error, stdout, stderr)
+	});		
+}
+
+
+exports.branchAtual = function(retorno){
+	exec( 'git branch' ,{ cwd: this.dir},function(error, stdout, stderr){
+		retorno(stdout);	
+		puts( error, stdout, stderr );
+		console.log( error, stdout, stderr)
+	});	
+}
+
+
+exports.listaBranch = function(retorno){
+	exec( 'git branch -a' ,{ cwd: this.dir},function(error, stdout, stderr){
+
+		var b = stdout;
+			b = b.toString("utf8");
+			b = b.split(/\n/)
+			b.pop()
+		retorno(b);			
+	});	
+}
